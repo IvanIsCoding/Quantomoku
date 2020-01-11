2aa9f3bf7bf8d368c3ca06f506a5a2a1180cf20b
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-01-11 13:57:32 -0800

    Initial client side commit

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="300px"/>
+    <MainComponent />
+  </div>
+</template>
+
+<script>
+import MainComponent from "./components/MainComponent.vue";
+
+export default {
+  name: "app",
+  components: {
+    MainComponent
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
