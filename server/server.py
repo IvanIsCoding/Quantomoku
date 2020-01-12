f18f7542575892af91d350e6f593847a32d49ec9
Ivan Carvalho <ivan.carvalho@alumni.ubc.ca>
2020-01-11 18:22:27 -0800

    Integrate socket.io and python-socketio

--- server/server.py
+++ server/server.py
@@ -1,14 +1,23 @@
-import eventlet
+from aiohttp import web
 import socketio
 
-sio = socketio.Server(async_mode='eventlet', cors_allowed_origins="*")
-app = socketio.WSGIApp(sio)
+sio = socketio.AsyncServer(cors_allowed_origins="*")
+app = web.Application()
+sio.attach(app)
 
 @sio.on('message')
 async def print_message(sid, message):
+    # When we receive a new event of type
+    # 'message' through a socket.io connection
+    # we print the socket ID and the message
     print("Socket ID: " , sid)
     print(message)
     await sio.emit('message', message[::-1])
 
+# We bind our aiohttp endpoint to our app
+# router
+#app.router.add_get('/', index)
+
+# We kick off our server
 if __name__ == '__main__':
-    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
+    web.run_app(app, port=8000)
